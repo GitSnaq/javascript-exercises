29576bc16fd66c06e60487143430c843887d90b9
GitSnaq <saqfire@gmail.com>
2022-05-08 10:41:14 +0200

    Made the reverseString function and passed the first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reverseStringArray = stringArray.reverse();
+    let reversedString = reverseStringArray.join("");
+    return reversedString;
 };
+
+
+/*
+
+
+
+
+*/
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
