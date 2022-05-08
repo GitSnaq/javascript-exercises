6f6a84fd05e8829c4ad5e8062552a574caf9a8d7
GitSnaq <saqfire@gmail.com>
2022-05-08 14:01:06 +0200

    Add checks for negative numbers and types other than numbers to return ERROR. All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if(num1<0 || num2<0){
+        return "ERROR"
+    }
+    if(typeof(num1) !== 'number' || typeof(num2) !== 'number'){
+        return "ERROR"
+    }
+    let largerNum = 0;
+    let smallerNum = 0;
+    if (num1 > num2){
+        largerNum = num1
+        smallernum = num2
+    }
+    else{
+       smallerNum = num1
+       largerNum = num2
+    }
+    total = ((largerNum-smallerNum)+1)*(smallerNum+largerNum)/2
+    return total
+}
 
 // Do not edit below this line
 module.exports = sumAll;
