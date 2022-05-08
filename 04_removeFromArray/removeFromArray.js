5bfd8671cbf3f5cf110839ef35b3141274d0972c
GitSnaq <saqfire@gmail.com>
2022-05-08 11:11:58 +0200

    Create removeArray function which takes in an array and removes a single given value and returns a new array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function (array, to_remove_items){
+    let filteredArray = [];
+    array.forEach(function(arrayElement){
+      if (arrayElement !== to_remove_items){
+       filteredArray.unshift(arrayElement)
+      }
+    })
+    filteredArray.reverse();
+    return filteredArray;
+  }
 // Do not edit below this line
 module.exports = removeFromArray;
