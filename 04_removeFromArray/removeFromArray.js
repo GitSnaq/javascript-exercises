a898155559ed8eeea33d4afe13e177c60bc8fa21
GitSnaq <saqfire@gmail.com>
2022-05-08 12:31:16 +0200

    Create a new function and comment out previous. Turns all arguments into an array and compares that against the array to be removed from

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,28 @@
-const removeFromArray = function (array, to_remove_items){
-    let filteredArray = [];
-    array.forEach(function(arrayElement){
-      if (arrayElement !== to_remove_items){
-       filteredArray.unshift(arrayElement)
-      }
-    })
-    filteredArray.reverse();
-    return filteredArray;
-  }
+const removeFromArray = function(array, to_remove_items){
+    var args = Array.prototype.slice.call(arguments);
+    return array.filter(v => {
+        return !args.includes(v);
+      });
+    }
+
+
+
+
+
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//The first iteration of the array function:
+
+// const removeFromArray = function (array, to_remove_items) {
+//     let filteredArray = [];
+//     array.forEach(function (arrayElement) {
+//         if (arrayElement !== to_remove_items) {
+//             filteredArray.unshift(arrayElement)
+//         }
+//     })
+//     filteredArray.reverse();
+//     return filteredArray;
+// }
